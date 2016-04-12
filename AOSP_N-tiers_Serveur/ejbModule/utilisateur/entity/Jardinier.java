22148d817cd5c906c842664cf279f09f40f0f972
Nico Lambert <nicolas.lambert.3d@gmail.com>
2016-04-12 16:29:54 +0200

    Merge branch utilisateur

--- AOSP_N-tiers_Serveur/ejbModule/utilisateur/entity/Jardinier.java
+++ AOSP_N-tiers_Serveur/ejbModule/utilisateur/entity/Jardinier.java
@@ -1,7 +1,13 @@
 package utilisateur.entity;
 
+import java.util.List;
+
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
+
+import potager.entity.Potager;
 
 @Entity
 @Table(name="aosp_jardinier")
@@ -13,7 +19,8 @@ public class Jardinier extends AbstractJardinier {
 	private static final long serialVersionUID = 1L;
 
 	
-	
+	@ManyToMany(mappedBy="visiteurs", 	fetch=FetchType.EAGER)
+	private List<Potager> potagerPartages;	
 	
 	public void clean() {
 		// TODO Auto-generated method stub
